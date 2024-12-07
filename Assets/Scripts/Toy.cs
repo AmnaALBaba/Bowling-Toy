0b6ff145dc51ca280a0fa19fe19a2a8f42d26123
Amna AL-Baba <amnaalbaba234@gmail.com>
2024-12-07 08:59:42 +0200

    first commit

---
+++ Assets/Scripts/Toy.cs
@@ -0,0 +1,97 @@
+using Unity.Mathematics;
+using UnityEngine;
+namespace Toy
+{
+    public class Toy : MonoBehaviour
+    {
+
+
+        bool isplayerOneActive = false;
+        bool isplayerTwoActive = false;
+
+        public Transform playerOneball;
+        public Transform playerTwoball;
+
+        float vInput;
+        float hInput;
+
+        public float speed = 1f;
+
+
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Play();
+            BallRest();
+
+        }
+
+
+
+        void PlayerInput(Transform ball)
+        {
+            vInput = Input.GetAxis("Vertical") * speed;
+            hInput = Input.GetAxis("Horizontal");
+            ball.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+            ball.transform.Translate(Vector3.left * hInput * Time.deltaTime);
+        }
+        void Play()
+        {
+            if (Input.GetKey(KeyCode.Alpha1))
+            {
+                isplayerOneActive = true;
+                isplayerTwoActive = false;
+
+                PlayerInput(playerOneball);
+
+
+                Debug.Log("Player One is active");
+
+
+            }
+            else if (Input.GetKey(KeyCode.Alpha2))
+            {
+                isplayerTwoActive = true;
+                isplayerOneActive = false;
+
+                PlayerInput(playerTwoball);
+                Debug.Log("Player Two is active");
+
+            }
+            else if (Input.GetKey(KeyCode.Alpha0))
+            {
+                isplayerOneActive = true;
+                isplayerTwoActive = true;
+                PlayerInput(playerOneball);
+                PlayerInput(playerTwoball);
+                Debug.Log("Player One & Player Two is active");
+
+
+            }
+
+        }
+        void BallRest()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (isplayerOneActive || isplayerTwoActive)
+                {
+                    playerOneball.transform.position = new Vector3(0f, 0.379999995f, 3.01600003f);
+
+
+
+
+                    playerTwoball.transform.position = new Vector3(3.75f,0.379999995f,3.01600003f);
+                }
+
+
+            }
+        }
+    }
+}
